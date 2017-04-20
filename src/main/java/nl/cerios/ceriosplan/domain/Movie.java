6f02ecd37b1fe431bac9a66a09217c98402e1c29
RonSanders <Ron.sanders@cerios.nl>
2017-04-20 12:08:51 +0200

    Eerste domain

---
+++ src/main/java/nl/cerios/ceriosplan/domain/Movie.java
@@ -0,0 +1,30 @@
+package nl.cerios.ceriosplan.domain;
+
+import java.math.BigInteger;
+
+public final class Movie {
+
+	private BigInteger movieId;
+	private String title;
+	private String description;
+
+	Movie(final BigInteger movieId, final String title, final String movieDescription) {
+		this.movieId = movieId;
+		this.title = title;
+		this.description = movieDescription;
+
+	}
+
+	public BigInteger getMovieId() {
+		return movieId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+}
